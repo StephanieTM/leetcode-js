0bee0a688e2c1395f7938d567f2b23643c36531e
Stephanie.Gao <tm1997@163.com>
2021-09-22 11:38:26 +0800

    feat: array - 11

---
+++ src/4.数组/[DONE]11.[ 剑指 Offer 03 ] 数组中重复的数字.js
@@ -0,0 +1,48 @@
+/**
+ * * 题目名称：数组中重复的数字
+ * * 题目地址：https://leetcode-cn.com/problems/shu-zu-zhong-zhong-fu-de-shu-zi-lcof
+
+找出数组中重复的数字。
+
+
+在一个长度为 n 的数组 nums 里的所有数字都在 0～n-1 的范围内。数组中某些数字是重复的，但不知道有几个数字重复了，也不知道每个数字重复了几次。请找出数组中任意一个重复的数字。
+
+示例 1：
+
+输入：
+[2, 3, 1, 0, 2, 5, 3]
+输出：2 或 3 
+ 
+
+限制：
+
+2 <= n <= 100000
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/shu-zu-zhong-zhong-fu-de-shu-zi-lcof
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+ */
+
+// * 思路：
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findRepeatNumber = function(nums) {
+  const map = new Map();
+  for (let i = 0; i < nums.length; i++) {
+    if (map.has(nums[i])) {
+      return nums[i];
+    }
+    map.set(nums[i], i);
+  }
+};
+
+// 测试用例
+let test = ''
+
+console.time('执行用时');
+console.log(xxx(test));
+console.timeEnd('执行用时');
