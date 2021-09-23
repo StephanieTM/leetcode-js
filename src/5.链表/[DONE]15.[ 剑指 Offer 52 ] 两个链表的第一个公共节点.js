63b315dd67b01f598ab7a2286a81b634e135ad9f
Stephanie.Gao <tm1997@163.com>
2021-09-23 18:12:01 +0800

    feat: linked list - 15

---
+++ src/5.链表/[DONE]15.[ 剑指 Offer 52 ] 两个链表的第一个公共节点.js
@@ -0,0 +1,135 @@
+/**
+ * * 题目名称：两个链表的第一个公共节点
+ * * 题目地址：https://leetcode-cn.com/problems/liang-ge-lian-biao-de-di-yi-ge-gong-gong-jie-dian-lcof
+
+输入两个链表，找出它们的第一个公共节点。
+
+如下面的两个链表：
+
+
+
+在节点 c1 开始相交。
+
+ 
+
+示例 1：
+
+
+
+输入：intersectVal = 8, listA = [4,1,8,4,5], listB = [5,0,1,8,4,5], skipA = 2, skipB = 3
+输出：Reference of the node with value = 8
+输入解释：相交节点的值为 8 （注意，如果两个列表相交则不能为 0）。从各自的表头开始算起，链表 A 为 [4,1,8,4,5]，链表 B 为 [5,0,1,8,4,5]。在 A 中，相交节点前有 2 个节点；在 B 中，相交节点前有 3 个节点。
+ 
+
+示例 2：
+
+
+
+输入：intersectVal = 2, listA = [0,9,1,2,4], listB = [3,2,4], skipA = 3, skipB = 1
+输出：Reference of the node with value = 2
+输入解释：相交节点的值为 2 （注意，如果两个列表相交则不能为 0）。从各自的表头开始算起，链表 A 为 [0,9,1,2,4]，链表 B 为 [3,2,4]。在 A 中，相交节点前有 3 个节点；在 B 中，相交节点前有 1 个节点。
+ 
+
+示例 3：
+
+
+
+输入：intersectVal = 0, listA = [2,6,4], listB = [1,5], skipA = 3, skipB = 2
+输出：null
+输入解释：从各自的表头开始算起，链表 A 为 [2,6,4]，链表 B 为 [1,5]。由于这两个链表不相交，所以 intersectVal 必须为 0，而 skipA 和 skipB 可以是任意值。
+解释：这两个链表不相交，因此返回 null。
+ 
+
+注意：
+
+如果两个链表没有交点，返回 null.
+在返回结果后，两个链表仍须保持原有的结构。
+可假定整个链表结构中没有循环。
+程序尽量满足 O(n) 时间复杂度，且仅用 O(1) 内存。
+本题与主站 160 题相同：https://leetcode-cn.com/problems/intersection-of-two-linked-lists/
+
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/liang-ge-lian-biao-de-di-yi-ge-gong-gong-jie-dian-lcof
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+ */
+
+// * 思路：
+
+// 思路1:hashMap存第一个链表的节点，再遍历第二个链表看是否存在于hashMap中
+
+// 思路2:双指针，链表A结束时去B的头，B结束时去A的头，看结束时是否相同
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} headA
+ * @param {ListNode} headB
+ * @return {ListNode}
+ */
+var getIntersectionNode1 = function(headA, headB) {
+  if (!headA || !headB) {
+    return null;
+  }
+
+  let p = headA;
+  const map = new Map();
+
+  while (p) {
+    map.set(p, p);
+    p = p.next;
+  }
+
+  p = headB;
+  while (p) {
+    if (map.has(p)) {
+      return p;
+    }
+    p = p.next;
+  }
+
+  return null;
+};
+
+// ----------------------------------------------------------------------------
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} headA
+ * @param {ListNode} headB
+ * @return {ListNode}
+ */
+var getIntersectionNode2 = function(headA, headB) {
+  if (!headA || !headB) {
+    return null;
+  }
+
+  let p1 = headA, p2 = headB;
+  while (p1 !== p2) {
+    p1 = p1 === null ? headB : p1.next;
+    p2 = p2 === null ? headA : p2.next;
+  }
+
+  return p1;
+};
+
+// 测试用例
+let test = ''
+
+console.time('执行用时');
+console.log(xxx(test));
+console.timeEnd('执行用时');
