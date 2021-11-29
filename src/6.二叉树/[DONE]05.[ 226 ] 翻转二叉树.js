64716efa7554386e683494c95bdebd10a03c6600
Stephanie.Gao <tm1997@163.com>
2021-11-29 10:26:47 +0800

    feat: binary tree - 05

---
+++ src/6.二叉树/[DONE]05.[ 226 ] 翻转二叉树.js
@@ -0,0 +1,39 @@
+/**
+ * * 题目名称：翻转二叉树
+ * * 题目地址：https://leetcode-cn.com/problems/invert-binary-tree
+
+翻转一棵二叉树。
+
+ */
+
+// * 思路：
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var invertTree = function(root) {
+  if (root === null) {
+    return null;
+  }
+  const left = invertTree(root.left);
+  const right = invertTree(root.right);
+  root.left = right;
+  root.right = left;
+  return root;
+};
+
+// 测试用例
+let test = ''
+
+console.time('执行用时');
+console.log(xxx(test));
+console.timeEnd('执行用时');
