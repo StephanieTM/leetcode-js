733427b2e1192b47670587b00e4419254b69b811
Stephanie.Gao <tm1997@163.com>
2021-12-13 18:35:56 +0800

    feat: binary tree - 36

---
+++ src/6.二叉树/[DONE]36.[ 1022 ] 从根到叶的二进制数之和.js
@@ -0,0 +1,73 @@
+/**
+ * * 题目名称：从根到叶的二进制数之和
+ * * 题目地址：https://leetcode-cn.com/problems/sum-of-root-to-leaf-binary-numbers
+
+给出一棵二叉树，其上每个结点的值都是 0 或 1 。每一条从根到叶的路径都代表一个从最高有效位开始的二进制数。例如，如果路径为 0 -> 1 -> 1 -> 0 -> 1，那么它表示二进制数 01101，也就是 13 。
+
+对树上的每一片叶子，我们都要找出从根到该叶子的路径所表示的数字。
+
+返回这些数字之和。题目数据保证答案是一个 32 位 整数。
+
+示例 1：
+输入：root = [1,0,1,0,1,0,1]
+输出：22
+解释：(100) + (101) + (110) + (111) = 4 + 5 + 6 + 7 = 22
+
+示例 2：
+输入：root = [0]
+输出：0
+
+示例 3：
+输入：root = [1]
+输出：1
+
+示例 4：
+输入：root = [1,1]
+输出：3
+
+提示：
+树中的结点数介于 1 和 1000 之间。
+Node.val 为 0 或 1 。
+
+ */
+
+// * 思路：
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var sumRootToLeaf = function(root) {
+  let result = 0;
+
+  const traverse = (node, num) => {
+    // 位运算快很多
+    num = (num << 1) + node.val;
+
+    if (!node.left && !node.right) {
+      result += num;
+    }
+
+    if (node.left) traverse(node.left, num);
+    if (node.right) traverse(node.right, num);
+  };
+
+  traverse(root, 0);
+
+  return result;
+};
+
+// 测试用例
+let test = ''
+
+console.time('执行用时');
+console.log(xxx(test));
+console.timeEnd('执行用时');
